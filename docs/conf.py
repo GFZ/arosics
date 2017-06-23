24a328027f674d105d4ca95a713caa71131d0c61
Daniel Scheffler <danschef@gfz-potsdam.de>
2017-06-24 00:44:52 +0200

    Updated CI builder.

--- docs/conf.py
+++ docs/conf.py
@@ -15,18 +15,13 @@
 
 import sys
 import os
-import site
 
 # If extensions (or modules to document with autodoc) are in another
 # directory, add these directories to sys.path here. If the directory is
 # relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath("../"))
-print(os.path.curdir)
-print()
-for p in sys.path:
-    print(p)
+
 # Get the project root dir, which is the parent dir of this
 cwd = os.getcwd()
 project_root = os.path.dirname(cwd)
@@ -35,9 +30,6 @@ project_root = os.path.dirname(cwd)
 # This lets us ensure that the source package is imported, and that its
 # version is used.
 sys.path.insert(0, project_root)
-
-for p in site.getsitepackages():
-    sys.path.append(p)
 
 import arosics
 
