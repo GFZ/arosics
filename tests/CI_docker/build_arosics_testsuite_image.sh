a75da7c850b3602d2b885e5cbc474b9939756826
Daniel Scheffler <danschef@gfz-potsdam.de>
2017-06-23 16:52:30 +0200

    Added TODOs to CI builder.

--- tests/CI_docker/build_arosics_testsuite_image.sh
+++ tests/CI_docker/build_arosics_testsuite_image.sh
@@ -5,11 +5,11 @@ dockerfile="arosics_ci.docker"
 tag="arosics_ci:latest"
 gitlab_runner="arosics_gitlab_CI_runner"
 
-# get py_tools_ds project
+# get py_tools_ds project # TODO move this to setup.py as soon as package is public
 rm -rf context/py_tools_ds
 git clone https://gitext.gfz-potsdam.de/danschef/py_tools_ds.git ./context/py_tools_ds
 
-# get geoarray project
+# get geoarray project # TODO move this to setup.py as soon as package is public
 rm -rf context/geoarray
 git clone https://gitext.gfz-potsdam.de/danschef/geoarray.git ./context/geoarray
 
