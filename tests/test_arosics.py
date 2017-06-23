0aef55b903c93d15da72760455b129d1891ba5cc
Daniel Scheffler <danschef@gfz-potsdam.de>
2017-06-23 17:37:10 +0200

    Updated Testcase.

--- tests/test_arosics.py
+++ tests/test_arosics.py
@@ -15,11 +15,6 @@ class TestArosics(unittest.TestCase):
 
     def setUp(self):
         """Set up test fixtures, if any."""
-        try:
-            from spectral.io import envi
-            print('Import worked during test!')
-        except ImportError:
-            print('Could not import spectral!!')
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
