bc38c9ae111c229fc8a5e257ac31e09f4c1cd1f3
Daniel Scheffler <danschef@gfz-potsdam.de>
2017-07-07 01:31:01 +0200

    updated __version__ and __versionalias__

--- arosics/__init__.py
+++ arosics/__init__.py
@@ -4,8 +4,8 @@
 
 __author__ = """Daniel Scheffler"""
 __email__ = 'daniel.scheffler@gfz-potsdam.de'
-__version__ = '0.3.9'
-__versionalias__ = '2017-07-04_10'
+__version__ = '0.3.10'
+__versionalias__ = '2017-07-07_02'
 
 
 from .CoReg import COREG
