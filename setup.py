14221f75a7d867b3edfa33de2845314897879a1b
Daniel Scheffler <danschef@gfz-potsdam.de>
2017-07-07 21:11:16 +0200

    Fix for holoviews setup issue within docker builder.
    updated __version__ and __versionalias__

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = ['coverage']
 
 setup(
     name='arosics',
-    version='0.4.4',
+    version='0.4.5',
     description="An Automated and Robust Open-Source Image Co-Registration Software for Multi-Sensor Satellite Data",
     long_description=readme + '\n\n' + history,
     author="Daniel Scheffler",
