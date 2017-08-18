edc2a6760a34b70995bed891d90a633bc73dfebc
Daniel Scheffler <danschef@gfz-potsdam.de>
2017-08-18 18:12:16 +0200

    Added version for docker container.

--- tests/CI_docker/build_arosics_testsuite_image.sh
+++ tests/CI_docker/build_arosics_testsuite_image.sh
@@ -3,7 +3,7 @@
 context_dir="./context"
 dockerfile="arosics_ci.docker"
 tag="arosics_ci:0.1"
-gitlab_runner="arosics_gitlab_CI_runner"
+gitlab_runner="arosics_gitlab_CI_runner_v0.1"
 
 echo "#### Build runner docker image"
 sudo docker rmi ${tag}
