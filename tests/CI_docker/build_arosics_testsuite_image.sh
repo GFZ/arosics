9103bd7b4b0f1ef426531a90f4640f47d6bb1940
Daniel Scheffler <danschef@gfz-potsdam.de>
2017-08-18 16:45:27 +0200

    Updated docker builder.

--- tests/CI_docker/build_arosics_testsuite_image.sh
+++ tests/CI_docker/build_arosics_testsuite_image.sh
@@ -2,7 +2,7 @@
 
 context_dir="./context"
 dockerfile="arosics_ci.docker"
-tag="arosics_ci:latest"
+tag="arosics_ci:0.1"
 gitlab_runner="arosics_gitlab_CI_runner"
 
 echo "#### Build runner docker image"
