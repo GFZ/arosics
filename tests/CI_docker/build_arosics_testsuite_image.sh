884cc1f0d266d80fce86a1ec80c3bc5095834746
Daniel Scheffler <danschef@gfz-potsdam.de>
2017-08-18 21:28:02 +0200

    Updated docker builder: coverage is now installed by conda.

--- tests/CI_docker/build_arosics_testsuite_image.sh
+++ tests/CI_docker/build_arosics_testsuite_image.sh
@@ -2,7 +2,7 @@
 
 context_dir="./context"
 dockerfile="arosics_ci.docker"
-tag="arosics_ci:0.2"
+tag="arosics_ci:latest"
 gitlab_runner="arosics_gitlab_CI_runner"
 
 echo "#### Build runner docker image"
