ad266ca1af3bb9458a0d59417dac5f4562eec513
Daniel Scheffler <danschef@gfz-potsdam.de>
2017-09-05 01:26:27 +0200

    Updated version info.

--- arosics/__init__.py
+++ arosics/__init__.py
@@ -4,8 +4,8 @@
 
 __author__ = """Daniel Scheffler"""
 __email__ = 'daniel.scheffler@gfz-potsdam.de'
-__version__ = '0.4.20'
-__versionalias__ = '2017-08-19_02'
+__version__ = '0.4.21'
+__versionalias__ = '2017-09-05_01'
 
 import warnings
 
