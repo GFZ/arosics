267bb54cf0764c8345e4450db70a3bc7d5354c10
Daniel Scheffler <danschef@gfz-potsdam.de>
2017-09-13 15:28:08 +0200

    Updated minimal version of py_tools_ds. Updated version info.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = ['numpy', 'gdal', 'shapely', 'scikit-image', 'matplotlib', 'geopandas', 'spectral', 'geoarray>=0.6.2',
-                'py_tools_ds>=0.7.0', 'plotly', 'cmocean', 'six',
+                'py_tools_ds>=0.7.1', 'plotly', 'cmocean', 'six',
                 # 'pykrige'  # conda install --yes -c conda-forge pykrige
                 # 'pyfftw', # conda install --yes -c conda-forge pyfftw=0.10.4 ; \
                 # 'basemap', # conda install --yes -c conda-forge basemap; \
@@ -27,7 +27,7 @@ test_requirements = requirements + ['coverage', 'nose', 'nose-htmloutput', 'redn
 
 setup(
     name='arosics',
-    version='0.4.31',
+    version='0.4.32',
     description="An Automated and Robust Open-Source Image Co-Registration Software for Multi-Sensor Satellite Data",
     long_description=readme + '\n\n' + history,
     author="Daniel Scheffler",
