c6a5807ba7c16bb7f0ac2310ebf235e0e16c2df2
Daniel Scheffler <danschef@gfz-potsdam.de>
2017-10-10 13:57:14 +0200

    Updated minimal versions of dependencies.

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['numpy', 'gdal', 'shapely', 'scikit-image', 'matplotlib', 'geopandas', 'spectral', 'geoarray>=0.6.2',
-                'py_tools_ds>=0.7.1', 'plotly', 'cmocean', 'six',
+requirements = ['numpy', 'gdal', 'shapely', 'scikit-image', 'matplotlib', 'geopandas', 'spectral', 'geoarray>=0.6.9',
+                'py_tools_ds>=0.9.0', 'plotly', 'cmocean', 'six',
                 # 'pykrige'  # conda install --yes -c conda-forge pykrige
                 # 'pyfftw', # conda install --yes -c conda-forge pyfftw=0.10.4 ; \
                 # 'basemap', # conda install --yes -c conda-forge basemap; \
--- tests/CI_docker/build_arosics_testsuite_image.sh
+++ tests/CI_docker/build_arosics_testsuite_image.sh
@@ -2,7 +2,7 @@
 
 context_dir="./context"
 dockerfile="arosics_ci.docker"
-tag="arosics_ci:0.4.30"
+tag="arosics_ci:0.5.1"
 gitlab_runner="arosics_gitlab_CI_runner"
 
 echo "#### Build runner docker image"
