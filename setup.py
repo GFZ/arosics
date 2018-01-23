ef28b03874fe3cbbe9006a8ea7e6b26f73c60303
Daniel Scheffler <danschef@gfz-potsdam.de>
2018-01-23 17:40:10 +0100

    Updated version info.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requirements = requirements + ['coverage', 'nose', 'nose-htmloutput', 'redn
 
 setup(
     name='arosics',
-    version='0.8.1',
+    version='0.8.2',
     description="An Automated and Robust Open-Source Image Co-Registration Software for Multi-Sensor Satellite Data",
     long_description=readme + '\n\n' + history,
     author="Daniel Scheffler",
