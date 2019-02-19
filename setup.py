193b7463ffa91bb15c13682d755803af6371af6d
Daniel Scheffler <danschef@gfz-potsdam.de>
2019-02-19 21:38:08 +0100

    Fixed PyPi upload error (invalid value for classifiers within setup.py).

--- setup.py
+++ setup.py
@@ -49,8 +49,8 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering'
-        'License :: OSI Approved :: GNU General Public License v3',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7',
