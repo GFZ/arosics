bbb213be7c9bfed853b2c84c0d37439076dee4ec
Daniel Scheffler <danschef@gfz-potsdam.de>
2020-04-08 13:37:47 +0200

    Updated version info.
    Signed-off-by: Daniel Scheffler <danschef@gfz-potsdam.de>

--- arosics/version.py
+++ arosics/version.py
@@ -22,5 +22,5 @@
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-__version__ = '0.9.11'
-__versionalias__ = '2020-04-07_06'
+__version__ = '0.9.12'
+__versionalias__ = '2020-04-08_01'
