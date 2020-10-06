82b1a74387fa8a08c6646f813c232914f6617fb5
Daniel Scheffler <danschef@gfz-potsdam.de>
2020-10-06 23:39:44 +0200

    Bumped version to 1.0.0.
    Signed-off-by: Daniel Scheffler <danschef@gfz-potsdam.de>

--- arosics/version.py
+++ arosics/version.py
@@ -22,5 +22,5 @@
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-__version__ = '0.9.26'
-__versionalias__ = '2020-10-02_01'
+__version__ = '1.0.0'
+__versionalias__ = '2020-10-06_01'
