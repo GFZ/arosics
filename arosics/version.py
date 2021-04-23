2db3866ea2eb37d506698c0bcd9869d801c9e232
Daniel Scheffler <danschef@gfz-potsdam.de>
2021-04-23 12:47:02 +0200

    Bumped version.
    Signed-off-by: Daniel Scheffler <danschef@gfz-potsdam.de>

--- arosics/version.py
+++ arosics/version.py
@@ -22,5 +22,5 @@
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-__version__ = '1.3.0'
-__versionalias__ = '2021-03-12_01'
+__version__ = '1.4.0'
+__versionalias__ = '2021-04-23_01'
