23b22fff9a2100e108a76abdbc90fddcdae3715b
Roelof van Dijk <roelof.vandijk@overstory.com>
2024-03-27 12:47:41 +0100

    perf: conditional nodata validation

--- arosics/Tie_Point_Grid.py
+++ arosics/Tie_Point_Grid.py
@@ -323,6 +323,7 @@ class Tie_Point_Grid(object):
             max_iter=self.COREG_obj.max_iter,
             max_shift=self.COREG_obj.max_shift,
             nodata=(self.COREG_obj.ref.nodata, self.COREG_obj.shift.nodata),
+            validate_nonempty=False,
             force_quadratic_win=self.COREG_obj.force_quadratic_win,
             binary_ws=self.COREG_obj.bin_ws,
             v=False,  # otherwise this would lead to massive console output
