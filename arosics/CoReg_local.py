ede1b98f6e25fafcf88c7bdcb2f4f9ca1be10fdf
Roelof van Dijk <roelof.vandijk@overstory.com>
2024-06-07 12:31:26 +0200

    perf: drop all unused bands

--- arosics/CoReg_local.py
+++ arosics/CoReg_local.py
@@ -323,6 +323,14 @@ class COREG_LOCAL(object):
         # resample input data in case there is a metadata rotation (not handled by AROSICS)
         self._check_and_handle_metaRotation()
 
+        # inside of COREG, only the match band is used. make objects smaller here.
+        if self.imref.bands > 1:
+            self.imref = self.imref.get_subset(zslice=slice(r_b4match-1, r_b4match))
+            r_b4match = 1
+        if self.im2shift.bands > 1:
+            self.im2shift = self.im2shift.get_subset(zslice=slice(r_b4match-1, r_b4match))
+            s_b4match = 1
+
         try:
             # ignore_errors must be False because in case COREG init fails, coregistration for the whole scene fails
             self.COREG_obj = COREG(self.imref, self.im2shift,
